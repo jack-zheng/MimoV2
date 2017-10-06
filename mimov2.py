44553583e71d921df3727a17e736c56a1e5e3c45
I306454 <jiabin.zheng01@sap.com>
2017-10-06 19:07:48 +0800

    test case of CURD and flask impl

--- mimov2.py
+++ mimov2.py
@@ -1,11 +1,92 @@
-from flask import Flask
+from flask import Flask, request, abort, jsonify
 
 app = Flask(__name__)
+
+
+# identify list object to store tasks
+tasks = [{'id':1, 'title':"t1"}]
 
 
 @app.route('/', methods=['GET'])
 def hello():
     return 'Hello World'
+
+
+@app.route('/todo/api/v1/tasks', methods=['POST'])
+def post_task():
+    if not request.get_json() or 'title' not in request.get_json():
+        abort(400)
+
+    # Generate a response
+    request_json = request.get_json()
+
+    task = {
+        'id': (tasks[-1]['id'] + 1 if tasks else 1),
+        'title': request_json.get('title'),
+        'description': 'N/A' if not request_json.get('description') else request_json.get('description'),
+        'status': 'In Progress' if not request_json.get('status') else request_json.get('status')
+    }
+
+    response = jsonify(task)
+    response.status_code = 201
+
+    tasks.append(task)
+    return response
+
+
+@app.route('/todo/api/v1/tasks', methods=['GET'])
+def get_tasks():
+    response = jsonify({'tasks':tasks})
+    response.status_code = 200
+    return response
+
+
+@app.route('/todo/api/v1/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    response = jsonify({'task':task})
+    response.status_code = 200
+    return response
+
+
+@app.route('/todo/api/v1/tasks', methods=['PUT'])
+def update_task():
+    if not request.get_json() or 'id' not in request.get_json():
+        abort(400)
+
+    task_id = request.get_json().get('id')
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+
+    item = task[0]
+    json_ret = request.get_json()
+    if json_ret.get('title'):
+        item['title'] = json_ret.get('title')
+
+    if json_ret.get('description'):
+        item['description'] = json_ret.get('description')
+
+    if json_ret.get('status'):
+        item['status'] = json_ret.get('status')
+    # now we only overwrite the value pass and keep others as before
+    
+    response = jsonify(item)
+    response.status_code = 201
+    return response
+
+
+@app.route('/todo/api/v1/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+
+    tasks.remove(task[0])
+    response = jsonify({"Remove": "Success"})
+    return response
 
 
 if __name__ == '__main__':
--- test_mimov2.py
+++ test_mimov2.py
@@ -1,5 +1,6 @@
 import unittest
 from mimov2 import app
+import json
 
 
 class MimoTestCase(unittest.TestCase):
@@ -7,10 +8,63 @@ class MimoTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
+        self.item01 = '{"title":"first target"}'
+
+        self.item02 = '{"title": "second target", "description": "my 2 target"}'
+
+        self.update = '{"id": id_replace, "title": "title update"}'
+
+        self.item_deletion = {'id': 1}
+
     # create a hello test as a start
     def test_hello(self):
         response = self.app.get('/')
         self.assertEqual(response.data.decode('utf-8'), 'Hello World')
+
+    # Post
+    def test_post_list(self):
+        resp = self.app.post('/todo/api/v1/tasks', data=self.item01, headers={"Content-type": "application/json"})
+        self.assertEqual(resp.status_code, 201)
+        self.assertIn('first target', str(resp.data))
+
+    # Get all
+    def test_get_list(self):
+        # post a new target first as data prepare.
+        resp= self.app.post('/todo/api/v1/tasks', data=self.item02, headers={"Content-type": "application/json"})
+        self.assertEqual(resp.status_code, 201)
+        resp = self.app.get('/todo/api/v1/tasks')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(2, len(json.loads(resp.data)['tasks']))
+
+    # Get one
+    def test_get_list_by_id(self):
+        resp = self.app.get('/todo/api/v1/tasks/{}'.format('1'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn('t1', str(resp.data))
+
+    # Update
+    def test_update_list(self):
+        update_id = self.get_task_id()
+        self.update = self.update.replace('id_replace', str(update_id))
+        resp = self.app.put('/todo/api/v1/tasks', data=self.update, headers={"Content-type": "application/json"})
+        self.assertEqual(resp.status_code, 201)
+        resp = self.app.get('/todo/api/v1/tasks')
+        self.assertIn('update', str(resp.data))
+
+    # Delete
+    def test_list_deletion(self):
+        find_id = self.get_task_id()
+
+        resp = self.app.delete('/todo/api/v1/tasks/{}'.format(find_id))
+        self.assertEqual(resp.status_code, 200)
+        self.assertNotIn(str(find_id), str(resp.data))
+
+    # get existing task id for test, use when delete, update testing
+    def get_task_id(self):
+        resp = self.app.get('/todo/api/v1/tasks')
+        json_tasks = json.loads(resp.data)['tasks']
+        find_id = json_tasks[0]['id']
+        return find_id
 
 
 if __name__ == '__main__':
