0a58ad3b934c97b8e6facac2c6ab0a3f5d82accb
I306454 <jiabin.zheng01@sap.com>
2017-10-07 11:19:57 +0800

    basic db work finish, integrate with api next

--- manage_db.py
+++ manage_db.py
@@ -1,24 +1,11 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from project import app, db
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
-    age = db.Column(db.String(123))
-    gender = db.Column(db.String(1))
 
 
 if __name__ == '__main__':
